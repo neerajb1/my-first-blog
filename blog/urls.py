bda84f896a934222dd5b8d3ba6202625468a29fc
Neeraj <shuklaneeraj2014.2015@gmail.com>
2018-01-13 01:45:29 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+
+#important for notification1
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]
